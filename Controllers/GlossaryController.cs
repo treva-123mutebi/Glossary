7f0526d9e37c84f623c0878baf1da13097d40b01
mutebidavidtrevor <mdavid.trevor@gmail.com>
2022-04-17 20:30:54 +0300

    final commit

---
+++ Controllers/GlossaryController.cs
@@ -0,0 +1,32 @@
+﻿//Controllers/GlossaryController.cs
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Glossary.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GlossaryController : ControllerBase
+    {
+        private static List<GlossaryItem> Glossary = new List<GlossaryItem> {
+            new GlossaryItem
+            {
+                Term= "Access Token",
+                Definition = "A credential that can be used by an application to access an API. It informs the API that the bearer of the token has been authorized to access the API and perform specific actions specified by the scope that has been granted."
+            },
+            new GlossaryItem
+            {
+                Term= "JWT",
+                Definition = "An open, industry standard RFC 7519 method for representing claims securely between two parties. "
+            },
+            new GlossaryItem
+            {
+                Term= "OpenID",
+                Definition = "An open standard for authentication that allows applications to verify users are who they say they are without needing to collect, store, and therefore become liable for a user’s login information."
+            }
+        };
+
+
+    }
+}
---
+++ GlossaryItem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Glossary
+{
+    public class GlossaryItem
+    {
+        //class defines Glossary item as a Term and its definition.
+        public string Term { get; set; }
+        public string Definition { get; set; }
+    }
+}
